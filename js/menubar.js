eeb8acc2b76d0dc24f7afe96f29f7e11610ad948
hontsev <hontsev@gmail.com>
2017-05-25 01:17:35 +0800

    w

--- js/menubar.js
+++ js/menubar.js
@@ -5,7 +5,7 @@ $(window).load(function(){
         '       项目 <b class="caret"></b>'+
         '    </a>'+
         '    <ul class="dropdown-menu">'+
-        '       <li><a href="game.html">游戏测试</a></li>'+
+        '       <li><a href="/game.html">游戏测试</a></li>'+
 		'	   <li><a href="/shaohuamoniqi/index.html">韶华模拟器</a></li>'+
 		'	   <li><a href="/ha2048/index.html">ha2048</a></li>'+
         '       <li class="divider"></li>'+
@@ -17,9 +17,18 @@ $(window).load(function(){
         '       测试 <b class="caret"></b>'+
         '    </a>'+
         '    <ul class="dropdown-menu">'+
-        '       <li><a href="filetest.html">文件操作测试</a></li>'+
-        '       <li><a href="formatchange.html">格式小工具</a></li>'+
+        '       <li><a href="/filetest.html">文件操作测试</a></li>'+
+        '       <li><a href="/formatchange.html">格式小工具</a></li>'+
 		'		<li><a href="http://neverwhere.esy.es/xsltml_2.1.2/mml2latex.html">mml转latex</a></li>'+
+        '    </ul>'+
+        ' </li>';
+		
+							str+='	     <li class="dropdown">'+
+        '    <a href="#" class="dropdown-toggle" data-toggle="dropdown">'+
+        '       文章 '+
+        '    </a>'+
+		'    <ul class="dropdown-menu">'+
+        '       <li><a href="/article/index.html">全部文章</a></li>'+
         '    </ul>'+
         ' </li>';
 		
